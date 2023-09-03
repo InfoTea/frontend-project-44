99685b154a1491b2ab8cf98b428c582bf30b9595
Anton Teplov <psyproffy@gmail.com>
2023-09-03 20:53:17 +0600

    import askName from src/cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+
+import askName from '../src/cli.js'
+
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
+
+const name = askName();
+console.log(`Hello, ${name}!`);
