b5d48004264d2e58b29e9c878388eacc7c2f8cc6
Anton Teplov <psyproffy@gmail.com>
2023-09-10 22:35:30 +0600

    fix bugs and added patch linter

--- Makefile
+++ Makefile
@@ -7,5 +7,5 @@ brain-games:
 publish:
 	npm publish --dry-run
 
-make lint:
-	npx eslint
+lint:
+	npx eslint .
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import askName from '../src/cli.js'
+import askName from '../src/cli';
 
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
