c7fd76f3441c3aaa0f0e2344e1ca6654cab0aced
Anton Teplov <psyproffy@gmail.com>
2023-09-12 18:41:09 +0600

    fix bugs with bin/brain/games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import askName from '../src/cli';
+import askName from '../src/cli.js';
 
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
