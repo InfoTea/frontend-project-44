065bfeef44386cab0fcd42011a067250e53d9e03
Anton Teplov <psyproffy@gmail.com>
2023-09-15 22:19:33 +0600

    added new have game-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import playAGame from '../src/game-play.js';
+
+const rule = 'Find the greatest common divisor of given numbers.';
+
+const gcd = (a, b) => (!b ? a : gcd(b, a % b));
+const randomNum = (n) => Math.cail(Math.random() * n);
+
+const howToPlay = () => {
+  const num1 = randomNum(20);
+  const num2 = randomNum(20);
+  const rightExtansion = `${num1} ${num2}`;
+
+  const rightAnswer = gcd(num1, num2);
+  return [rightExtansion, rightAnswer];
+};
+
+playAGame(rule, howToPlay);
