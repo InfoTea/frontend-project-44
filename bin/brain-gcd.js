9f61634b1467223685e686725927420ad11d1867
Anton Teplov <psyproffy@gmail.com>
2023-09-15 22:22:43 +0600

    fixed bug in brain-gcd.js with wrong name of function

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -5,14 +5,14 @@ import playAGame from '../src/game-play.js';
 const rule = 'Find the greatest common divisor of given numbers.';
 
 const gcd = (a, b) => (!b ? a : gcd(b, a % b));
-const randomNum = (n) => Math.cail(Math.random() * n);
+const randomNum = (n) => Math.ceil(Math.random() * n);
 
 const howToPlay = () => {
   const num1 = randomNum(20);
   const num2 = randomNum(20);
   const rightExtansion = `${num1} ${num2}`;
 
-  const rightAnswer = gcd(num1, num2);
+  const rightAnswer = gcd(num1, num2).toString();
   return [rightExtansion, rightAnswer];
 };
 
